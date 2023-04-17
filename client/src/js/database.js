7334a59a9fa524232d4e6adc5da56f89d3206ca1
Dustin Honeck <honeck_34@hotmail.com>
2023-04-16 19:06:12 -0500

    Add indexedDB functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,18 +4,36 @@ const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        console.log('JATE database already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      console.log('JATE database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async function (content) {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ value: content });
+  const result = await request;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async function () {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+
+  if (result.length == 0) { // Returns false if no results are found
+    console.log('No previous edits - show JATE header');
+    return false;
+  } else { // Returns the last item if results are found
+    console.log('Previous edit found - show results');
+    return result[result.length - 1].value;
+  }
+};
 
 initdb();
